b91400d5025b0b06e9c34b1d13fb0b506a6a41d4
dvkeia <devikak475@gmail.com>
2023-03-07 12:19:43 -0500

    Updated decoder func

--- lab06.py
+++ lab06.py
@@ -7,29 +7,43 @@ Original file is located at
     https://colab.research.google.com/drive/1t2wRfdzIN2vUIz6WFMKb0dCuksTgdC2v
 """
 
+
 def encoder(password):
-  rst = ""
-  for i in password:
-    rst += str(int(i)+3)
-  return rst
+    rst = ""
+    for i in password:
+        k = (int(i) + 3) % 10
+        rst += str(k)
+    return rst
+
+
+def decoder(password):
+    result = ""
+    for i in password:
+        val = int(i)
+        if (val - 3) < 0:
+            val = (val + 10) - 3
+        result = result + str(val)
+    return result
+
 
 # Encoder
 def print_menu():
-  print("Menu")
-  print("-------------")
-  print("1. Encode")
-  print("2. Decode")
-  print("3. Quit")
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
 
 while True:
-  print_menu()
-  opt = int(input("Please enter an option: "))
-  store = ""
-  if opt == 1:
-    password = input("Please enter your password to encode: ")
-    store = encoder(password)
-    print("Your password has been encoded and stored! ")
-
-  if opt == 3:
-    break
-
+    print_menu()
+    opt = int(input("Please enter an option: "))
+    store = ""
+    if opt == 1:
+        password = input("Please enter your password to encode: ")
+        store = encoder(password)
+        print("Your password has been encoded and stored! ")
+    elif opt == 2:
+        print(f'The encoded password is {store}, and the original password is {decoder(store)}')
+    elif opt == 3:
+        break
